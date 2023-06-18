0c19cb8493f2884512c4de1d9c32e9b7e33adbb6
realyutou <ssps9430@gmail.com>
2023-06-18 10:47:37 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results})
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant})
+})
+
+app.get('/search',(req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurants => restaurants.name.toLowerCase().includes(keyword.toLowerCase()) || restaurants.category.includes(keyword))
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`)
+})
