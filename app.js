67e64d47e6f647711756b8d36c8ce3a685452a2f
realyutou <ssps9430@gmail.com>
2023-06-23 16:19:07 +0800

    新增編輯功能及排版優化

--- app.js
+++ app.js
@@ -70,6 +70,35 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 使用者可以編輯一家餐廳的詳細資訊
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant: restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
 // 使用者可以依餐廳名稱或類別搜尋特定餐廳
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
